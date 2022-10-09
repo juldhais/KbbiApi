f5c5dcb668c26af99a33d5cd9c92e1cb9018d81c
Juldhais Hengkyawan <juldhais@gmail.com>
2022-10-09 19:41:22 +0700

    Add sqlite db

---
+++ KbbiApi/KbbiContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace KbbiApi;
+
+public class KbbiContext : DbContext
+{
+    public KbbiContext(DbContextOptions<KbbiContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Dictionary> Dictionary { get; set; }
+}
+
+public class Dictionary
+{
+    public int Id { get; set; }
+    public string Word { get; set; }
+    public List<Definition> Definitions { get; set; }
+}
+
+public class Definition
+{
+    public int Id { get; set; }
+    public Dictionary Dictionary { get; set; }
+    public int DictionaryId { get; set; }
+    public string Description { get; set; }
+}
--- KbbiApi/Program.cs
+++ KbbiApi/Program.cs
@@ -1,6 +1,11 @@
 using HtmlAgilityPack;
+using KbbiApi;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<KbbiContext>(opt =>
+    opt.UseSqlite(builder.Configuration.GetConnectionString("Sqlite")));
 
 builder.Services.AddCors(options =>
     options.AddDefaultPolicy(policy => 
@@ -9,23 +14,35 @@ builder.Services.AddCors(options =>
             .AllowAnyOrigin()));
 
 var app = builder.Build();
+
+var db = app.Services.CreateScope().ServiceProvider.GetRequiredService<KbbiContext>();
+db.Database.EnsureCreated();
+
 app.UseHttpsRedirection();
 app.UseFileServer();
 app.UseCors();
 
-const string baseUrl = "https://kbbi.kemdikbud.go.id";
+const string baseUrl = "https://kbbi.kemdikbud.go.id/entri";
 var htmlWeb = new HtmlWeb();
 
-app.MapGet("/{kata}", (string kata) =>
+app.MapGet("/words/{kata}", async (string kata, CancellationToken cancellationToken, KbbiContext context) =>
 {
-    var url = $"{baseUrl}/entri/{kata}";
+    // find data in the local database first
+    var dict = await context.Dictionary.AsNoTracking()
+        .Include(x => x.Definitions)
+        .FirstOrDefaultAsync(x => x.Word == kata.ToLower(), cancellationToken);
+    if (dict != null)
+        return new WordResponse(kata, dict.Definitions.Select(x => x.Description));
+    
+    // scrap data from kbbi website
+    var url = $"{baseUrl}/{kata}";
     var doc = htmlWeb.Load(url);
     var h2Nodes = doc.DocumentNode.SelectNodes("//h2");
 
     if (h2Nodes == null)
-        return new ResponseModel(kata, Enumerable.Empty<string>());
+        return new WordResponse(kata, Enumerable.Empty<string>());
 
-    var listArti = new List<string>();
+    var definitions = new List<string>();
     foreach (var h2 in h2Nodes)
     {
         var liNodes = h2?.NextSibling?.NextSibling?.ChildNodes;
@@ -44,13 +61,28 @@ app.MapGet("/{kata}", (string kata) =>
             // remove pollution (ex: v Huk, n, v cak, etc)
             var arti = innerText.Split("    ").Last().Trim();
             
-            listArti.Add(arti);
+            definitions.Add(arti);
         }
     }
     
-    return new ResponseModel(kata, listArti);
+    // save to local database
+    if (definitions.Count > 0)
+    {
+        var newWord = new Dictionary
+        {
+            Word = kata.ToLower(),
+            Definitions = definitions.Select(x => new Definition
+            {
+                Description = x
+            }).ToList()
+        };
+        context.Dictionary.Add(newWord);
+        await context.SaveChangesAsync(cancellationToken);
+    }
+    
+    return new WordResponse(kata, definitions);
 });
 
 app.Run();
 
-public record ResponseModel(string Kata, IEnumerable<string> Arti);
+public record WordResponse(string Word, IEnumerable<string> Definitions);
